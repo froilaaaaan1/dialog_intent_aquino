74924c85942776c253fc780f3ee8ecd87ccae042
froilan <froilanaquino1@gmail.com>
2023-11-13 12:41:00 +0800

    added custom strings for activity 1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "com.example.dialog_and_intent_aquino_froilan"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.dialog_and_intent_aquino_froilan"
