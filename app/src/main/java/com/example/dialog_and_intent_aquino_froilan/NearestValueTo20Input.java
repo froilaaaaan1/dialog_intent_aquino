5a6411f323096ade699afd509de61413fe8e2c64
froilan <froilanaquino1@gmail.com>
2023-11-13 20:28:31 +0800

    cleanse and make the function lambda for cleaner code

--- app/src/main/java/com/example/dialog_and_intent_aquino_froilan/NearestValueTo20Input.java
+++ app/src/main/java/com/example/dialog_and_intent_aquino_froilan/NearestValueTo20Input.java
@@ -1,8 +1,6 @@
 package com.example.dialog_and_intent_aquino_froilan;
 
-import android.content.DialogInterface;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -21,27 +19,36 @@ public class NearestValueTo20Input extends AppCompatActivity {
         EditText thirdTextfield = (EditText) findViewById(R.id.thirdNumberTextField);
         Button getNearest = (Button) findViewById(R.id.getNearest);
 
-        getNearest.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                int firstNumber = Integer.parseInt(firstTextfield.getText().toString());
-                int secondNumber = Integer.parseInt(secondTextfield.getText().toString());
-                int thirdNumber = Integer.parseInt(thirdTextfield.getText().toString());
-
-                AlertDialog.Builder prompt = new AlertDialog.Builder(NearestValueTo20Input.this);
-                prompt.setTitle("Nearest to 20");
-                prompt.setMessage("Do you want to discard?");
-                prompt.setCancelable(false);
-                prompt.show();
-                prompt.setPositiveButton("YES", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        Toast note = new Toast(NearestValueTo20Input.this);
-                        note.setText(R.string.process_done);
-                        note.show();
-                    }
-                });
+        getNearest.setOnClickListener(v -> {
+            int firstNumber = Integer.parseInt(firstTextfield.getText().toString());
+            int secondNumber = Integer.parseInt(secondTextfield.getText().toString());
+            int thirdNumber = Integer.parseInt(thirdTextfield.getText().toString());
+            int[] numbers = {firstNumber, secondNumber, thirdNumber};
+            int nearest = 2147483647;
+            for (int number : numbers) {
+                if (Math.abs(number - 20) < nearest) {
+                    nearest = number;
+                }
             }
+
+            AlertDialog.Builder prompt = new AlertDialog.Builder(NearestValueTo20Input.this);
+            prompt.setTitle("Nearest to 20");
+            prompt.setCancelable(false);
+            prompt.setMessage(String.valueOf(nearest));
+            prompt.setPositiveButton("DONE", (dialog, which) -> {
+                Toast note = new Toast(NearestValueTo20Input.this);
+                firstTextfield.setText("");
+                secondTextfield.setText("");
+                thirdTextfield.setText("");
+                note.setText(R.string.process_done);
+                note.show();
+            });
+            prompt.setNegativeButton("RETYPE", (dialog, which) -> {
+
+            });
+
+            prompt.show();
+
         });
     }
 }
