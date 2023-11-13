f7d6846e6a6d2a50c09dd828016274e79aba30d8
froilan <froilanaquino1@gmail.com>
2023-11-13 11:34:12 +0800

    Initialize Activities

---
+++ app/src/main/java/com/example/dialog_and_intent_aquino_froilan/ChineseZodiacAnimalInput.java
@@ -0,0 +1,14 @@
+package com.example.dialog_and_intent_aquino_froilan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChineseZodiacAnimalInput extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chinese_zodiac_animal_input);
+    }
+}
---
+++ app/src/main/java/com/example/dialog_and_intent_aquino_froilan/NearestValueTo20Input.java
@@ -0,0 +1,14 @@
+package com.example.dialog_and_intent_aquino_froilan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NearestValueTo20Input extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nearest_value_to20_input);
+    }
+}
