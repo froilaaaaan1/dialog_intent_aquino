953fcf35a6ffa2088627c81170d9615d2a545403
froilan <froilanaquino1@gmail.com>
2023-11-14 01:20:27 +0800

    done

--- app/src/main/java/com/example/dialog_and_intent_aquino_froilan/ChineseZodiacAnimalResult.java
+++ app/src/main/java/com/example/dialog_and_intent_aquino_froilan/ChineseZodiacAnimalResult.java
@@ -1,8 +1,10 @@
 package com.example.dialog_and_intent_aquino_froilan;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class ChineseZodiacAnimalResult extends AppCompatActivity {
 
@@ -10,5 +12,25 @@ public class ChineseZodiacAnimalResult extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chinese_zodiac_animal_result);
+        Intent intentObject = getIntent();
+        final String NAME = intentObject.getStringExtra("name");
+        final int AGE = intentObject.getIntExtra("age", 0);
+        final String BIRTHPLACE = intentObject.getStringExtra("birthplace");
+        final String GENDER = intentObject.getStringExtra("gender");
+        final int BIRTH_YEAR = intentObject.getIntExtra("birth_year", 0);
+        final String SIGN = intentObject.getStringExtra("sign");
+        TextView nameTextView = findViewById(R.id.nameField);
+        TextView ageTextView = findViewById(R.id.ageField);
+        TextView birthplaceTextView = findViewById(R.id.birthplaceField);
+        TextView genderTextView = findViewById(R.id.genderField);
+        TextView birthYearTextView = findViewById(R.id.birthYearField);
+        TextView zodiacAnimal = findViewById(R.id.zodiacAnimalField);
+
+        nameTextView.setText(NAME);
+        ageTextView.setText(String.valueOf(AGE));
+        birthplaceTextView.setText(BIRTHPLACE);
+        genderTextView.setText(GENDER);
+        birthYearTextView.setText(String.valueOf(BIRTH_YEAR));
+        zodiacAnimal.setText(SIGN);
     }
 }
